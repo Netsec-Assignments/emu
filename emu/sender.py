bc98935ccea1f7ea6cdd6cae2ac6598017a16b01
mat <siwoski.mateusz@gmail.com>
2016-11-20 11:24:55 -0800

    modified sender.py

--- emu/sender.py
+++ emu/sender.py
@@ -1,48 +1,80 @@
-import os
+import emu.packet as packet
 import socket
+import os
 import sys
+import json
+from enum import Enum
 
-FILE_NAME = "config.txt"
+# return values from run() to indicate what the controlling program should do
+SWITCH = 0
+DONE = 1
+
+class Sender:
+    def __init__(self, sock, port, emulator):
+        self.sock = sock
+        self.port = port
+        self.emulator = emulator
+        self.ack_num = 0
+        self.seq_num = 0
+
+    def run(self):
+        print("run")
 
 class Client:
-    def __init__(self, ipAddr, portNum):
-        if(ipAddr == None):
-            self.ipAddr = '192.168.0.7'
-        else:
-            self.ipAddr = ipAddr
-        if(portNum == None):
-            self.portNum = "7000"
-        else:
-            self.portNum = int(portNum)
-        self.running = True
-        print("Client Initialized")
-
+    def __init__(self, cfg_file_path, is_receiver):
+        if(not os.path.isfile(cfg_file_path)):
+            raise TypeError("cfg_file_path must point to an existing file")
+        
+        with open(cfg_file_path) as config_file:    
+            self.config = json.load(config_file)
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(self.config["timeout"])
+        self.is_sender = is_sender
+       
     def start(self):
         print("run executed")
-
+        while(True):
+            if(self.is_sender):
+                sender = Sender(self.sock, self.config["port"], self.config["emulator"])
+                result = sender.run()
+                if(result == DONE):
+                    return
+                else:
+                    self.is_sender = False
+            else:
+                pass
         #connect to channel
-        self.dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.dataSock.connect((self.ipAddr, self.portNum))
-        self.dataSock.setblocking(0)
-        print("Disconnected.")
-        self.dataSock.close()
+        #self.dataSock.connect((self.ipAddr, self.portNum))
+        #self.dataSock.setblocking(0)
+        #print("Disconnected.")
+        #self.dataSock.close()
+        
 
-def read_config():
-    with open(FILE_NAME) as f:
-        content = f.read().splitlines()
-    print("    User will connect on :", content[0])
-    print("    on port:", content[1])
-    return content
+    def intro():
+        print("    Final Assignment: C7005")
+        print("    Mat Siwoski and Shane Spoor\n")
+        print("    Started the Client Program")
 
-def intro():
-    print("    Final Assignment: C7005")
-    print("    Mat Siwoski and Shane Spoor\n")
-    print("    Started the Client Program")
+if(__name__ == "__main__"):
+    # check whether we're supposed to receive or send first based on cmd-line args
+    # start host in appropriate mode 
+    if(len(sys.argv) != 3):
+        print("usage: host <config file> [receiver|sender]")
+        sys.exit(1)
 
-intro()
-content = read_config()  
+    is_sender = False
+    if(sys.argv[2] == "receiver"):
+        is_sender = False
+    elif(sys.argv[2] == "sender"):
+        is_sender = True
+    elif(sys.argv[2] != "sender"):
+        print("usage: host <config file> [receiver|sender]")
+        sys.exit(1)
 
-
-client = Client(content[0], content[1])
-
-client.start();
+    try:
+        h = Host(sys.argv[1], is_sender)
+        h.intro()
+        h.start()
+    except TypeError as err:
+        print(str(err))
