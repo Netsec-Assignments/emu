116b4cbfbe9a37c7265b51fe12b28179e6ebaee0
mat <siwoski.mateusz@gmail.com>
2016-11-23 22:30:07 -0800

    Modified the sender, fixed minor connection issues

--- emu/sender.py
+++ emu/sender.py
@@ -12,11 +12,14 @@ DONE = 1
 
 class Sender:
     def __init__(self, sock, ip, port, emulator):
+        #Config variables
         self.sock = sock
         self.ip = ip
         self.port = port
         self.emulator = emulator
+        self.window_size = window_size
 
+        #State variables
         self.ack_num = 0
         self.seq_num = 0
         self.is_done = False
@@ -34,16 +37,19 @@ class Sender:
                     return None
                 else:
                     continue
-            if(addr == self.emulator):
+            if(addr[0] == self.emulator):
                 return packet.unpack_packet(pkt)
             
     """We'll stay in this state until receiving a SYN or FIN"""
     def wait_for_syn_ack(self):
         pkt = self.wait_for_packet(False)
         if(pkt.flags == packet.Type.FIN):
+            print("received FIN packet, finishing up")
             self.is_done = True
-            self.finish_status = DONE
-        elif(pkt.type == (packet.Type.SYN or packet.Type.ACK)):
+            self.finish_status = DONE            
+        elif(pkt.flags == (packet.Type.SYN or packet.Type.ACK)):
+            print("received SYN_ACK packet: responding with ACK")
+            self.latest_ack = packet.
             self.ack_num += rcvd_window_bytes
 
 
@@ -71,17 +77,30 @@ class Sender:
     """Begin sending data to the server """    
     def send_data(self):
         print("test")
+
+        self.sock.sendto(response, (self.emulator, self.port))
+        
         
     def run(self):
+        print("starting the connection")
         self.sock.connect((self.ip, self.port))
         self.send_syn()
-        if (self.is_done):
+        if(self.is_done):
+            print("finished running, returning status {}".format("SWITCH" if self.finish_status == SWITCH else "DONE"))
             return self.finish_status
 
-        byte_buf = []
+        print("waiting for SYN_ACK packet...")
+        self.wait_for_syn_ack()
+        if(self.is_done):
+            print("finished running, returning status {}".format("SWITCH" if self.finish_status == SWITCH else "DONE"))
+            return self.finish_status
+
+        print("entering main sender loop")
         while(not self.is_done):
-           self.wait_for_syn_ack()
-            
+            self.send_data()
+
+        print("finished running, returning status {}".format("SWITCH" if self.finish_status == SWITCH else "DONE"))
+        return self.finish_status            
 
 class Client:
     def __init__(self, cfg_file_path, is_receiver):
