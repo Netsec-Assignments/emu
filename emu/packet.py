eb0b102dcaebc4f62e4d62cb1ccddf43e3339b30
Shane-S <shane.d.spoor@gmail.com>
2016-11-27 15:41:59 -0800

    Fix eot_ack_packet creation in packet

--- emu/packet.py
+++ emu/packet.py
@@ -89,7 +89,7 @@ def create_synack_packet(syn_packet):
 def create_eot_packet():
     return Packet(Type.EOT, 0, 0, None)
 
-def create_eot_ack_pack():
+def create_eot_ack_packet():
     return Packet(Type.EOT | Type.ACK, 0, 0, None)
 
 def create_fin_packet():
