e3ffa8739d8676afc23bcd0759d3fc252e764744
Shane-S <shane.d.spoor@gmail.com>
2016-11-27 13:09:54 -0800

    Add test files, read from file in sender, fix SO_REUSEADDR

--- emu/sender.py
+++ emu/sender.py
@@ -11,7 +11,7 @@ SWITCH = 0
 DONE = 1
 
 class Sender:
-    def __init__(self, sock, port, emulator, window_size):
+    def __init__(self, sock, port, emulator, window_size, read_data, file_size):
         #Config variables
         self.sock = sock
         self.port = port
@@ -26,6 +26,13 @@ class Sender:
         self.ack_now = False
         self.finish_status = DONE
         self.latest_ack = None
+        self.current_offset = 0
+        self.bytes_left = 0
+        self.bytes_to_read = 0
+    
+        #File info
+        self.read_data = read_data
+        self.file_size = file_size
 
     def wait_for_packet(self, return_on_timeout = True):
         while(True):
@@ -82,7 +89,12 @@ class Sender:
         
     """Begin sending data to the server """    
     def send_data(self):
-        packets = packet.create_data_packets(bytes([128] * packet.MAX_DATA_LENGTH * self.window_size), self.seq_num)
+        self.current_offset = self.seq_num - 1
+        self.bytes_left = self.file_size - self.current_offset
+        self.bytes_to_read = min(self.bytes_left, packet.MAX_DATA_LENGTH * self.window_size)
+
+        end = self.current_offset + self.bytes_to_read
+        packets = packet.create_data_packets(bytes(self.read_data[self.current_offset:end]), self.seq_num)
         for p in packets:
             self.sock.sendto(packet.pack_packet(p), (self.emulator, self.port))
             print("sent DATA packet with seq {} and data len {} to {} on port {}".format(p.seq_num, p.data_len, self.emulator, self.port))
@@ -124,23 +136,34 @@ class Sender:
         return self.finish_status            
 
 class Client:
-    def __init__(self, cfg_file_path, is_receiver):
+    def __init__(self, cfg_file_path, is_receiver, file_path):
         if(not os.path.isfile(cfg_file_path)):
             raise TypeError("cfg_file_path must point to an existing file")
         
+        if(not os.path.isfile(file_path)):
+            raise TypeError("file_path must point to an existing file")
+        
         with open(cfg_file_path) as config_file:    
             self.config = json.load(config_file)
-        
+
+        with open(file_path, 'r') as read_file:
+            self.read_data = read_file.read()
+
+        if os.path.isfile(file_path):
+            file_info = os.stat(file_path)
+        self.file_size = file_info.st_size
+        print("file size {}".format(self.file_size))
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('', self.config["port"]))
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', self.config["port"]))
         self.sock.settimeout(self.config["timeout"])
         self.is_sender = is_sender
        
     def start(self):
         while(True):
             if(self.is_sender):
-                sender = Sender(self.sock, self.config["port"], self.config["emulator"], self.config["window_size"])
+                sender = Sender(self.sock, self.config["port"], self.config["emulator"], self.config["window_size"], self.read_data, self.file_size)
 
                 result = sender.run()
                 if(result == DONE):
@@ -158,21 +181,21 @@ class Client:
 if(__name__ == "__main__"):
     # check whether we're supposed to receive or send first based on cmd-line args
     # start host in appropriate mode 
-    if(len(sys.argv) != 3):
-        print("usage: host <config file> [receiver|sender]")
+    if(len(sys.argv) != 4):
+        print("usage: host <config file> <file path> [sender]")
         sys.exit(1)
 
     is_sender = False
-    if(sys.argv[2] == "receiver"):
+    if(sys.argv[3] == "receiver"):
         is_sender = False
-    elif(sys.argv[2] == "sender"):
+    elif(sys.argv[3] == "sender"):
         is_sender = True
-    elif(sys.argv[2] != "sender"):
-        print("usage: host <config file> [receiver|sender]")
+    elif(sys.argv[3] != "sender"):
+        print("usage: host <config file> <file path> [sender]")
         sys.exit(1)
 
     try:
-        c = Client(sys.argv[1], is_sender)
+        c = Client(sys.argv[1], is_sender, sys.argv[2])
         c.intro()
         c.start()
     except TypeError as err:
